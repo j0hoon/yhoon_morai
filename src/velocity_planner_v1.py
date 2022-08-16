399a8b347195e0bdda45e095f9f635f7654d35f4
j0hoon <riverwind97@gmail.com>
2022-08-16 17:59:33 +0900

    0816 퇴근

--- src/velocity_planner_v1.py
+++ src/velocity_planner_v1.py
@@ -27,9 +27,50 @@ class velocityPlanning:
         # 2. 
 
         v_min = 10
-        desiredVelocity = []
+        out_vel_plan = []
+
+        for i in range(0,point_num):
+            out_vel_plan.append(self.car_max_speed)
 
         if local_path.deg > 60: # 임의로 정한 각도임 TODO 이부분 수정해야함
-            desiredVelocity.append(v_min)
+            
+            out_vel_plan.append(v_min)
+
+        for i in range(point_num, len(global_path.poses) - point_num):
+            x_list = []
+            y_list = []
+            for box in range(-point_num, point_num):
+                x = global_path.poses[i+box].pose.position.x
+                y = global_path.poses[i+box].pose.position.y
+                x_list.append([-2*x, -2*y ,1])
+                y_list.append((-x*x) - (y*y))
+
+            #TODO: (5) 도로의 곡률 계산
+            x_matrix = np.array(x_list)
+            y_matrix = np.array(y_list)
+            x_trans = x_matrix.T
+
+            a_matrix = np.linalg.inv(x_trans.dot(x_matrix)).dot(x_trans).dot(y_matrix)
+            a = a_matrix[0]
+            b = a_matrix[1]
+            c = a_matrix[2]
+            r = sqrt(a*a+b*b-c)
+
+            #TODO: (6) 곡률 기반 속도 계획
+            v_max = sqrt(r*9.8*self.road_friction)
+
+            if v_max > self.car_max_speed:
+                v_max = self.car_max_speed
+            out_vel_plan.append(v_max)
+        
+
+        for i in range(len(global_path.poses) - point_num, len(global_path.poses)-10):
+            out_vel_plan.append(30)
+
+        for i in range(len(global_path.poses) - 10, len(global_path.poses)):
+            out_vel_plan.append(0)
+
+        return out_vel_plan       
+
         
 
